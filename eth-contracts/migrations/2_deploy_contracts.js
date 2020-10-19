09476f14d7a0afef1194ffcbb3840a1758d01cdc
Mario Briseño <mario.briseno7@gmail.com>
2020-10-18 19:00:31 -0500

    making migration work properly

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -2,9 +2,9 @@
 var SquareVerifier = artifacts.require("./Verifier.sol");
 var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
-let deployAddress = "0xf17f52151EbEF6C7334FAD080c5704D77216b732";
+let deployAddress = "0x63c21e076cd65Fff647D0396B1Ff641EAE9A4DB0";
 module.exports = function(deployer) {
   deployer.deploy(SquareVerifier);
-  deployer.deploy(SolnSquareVerifier, deployAddress);
+  deployer.deploy(SolnSquareVerifier, SquareVerifier.address);
 };
 
